4f63648b0e2c2a0146ea12a3fac5235bbad21ab4
Sanjay <727722eucs149@skcet.ac.in>
2025-09-27 11:11:06 +0530

    one entry integration

---
+++ lib/oneentry.ts
@@ -0,0 +1,49 @@
+import retriveRefreshToken from "@/actions/auth/retrieveRefreshToken";
+import storeRefreshToken from "@/actions/auth/storeRefreshToken";
+import { defineOneEntry } from "oneentry";
+
+
+export type ApiClientType = ReturnType<typeof defineOneEntry> | null;
+
+let apiClient: ApiClientType = null;
+
+
+async function setupApiClient(): Promise<ReturnType<typeof defineOneEntry>> {
+    const apiUrl = process.env.PROJECTID_URL;
+
+    if(!apiUrl)
+    {
+        throw new Error('PROJECT ID URL IS MISSING');
+    }
+
+    if(!apiClient)
+    {
+        try{
+            const refreshToken = await retriveRefreshToken();
+            apiClient = defineOneEntry(apiUrl,{token:process.env.ONEENTRY_TOKEN,langCode: 'en-US',
+                auth:{refreshToken:refreshToken|| undefined,customAuth:false,
+                    saveFunction:async (newToken: string)=>{await storeRefreshToken(newToken);},},});
+        }
+        catch(error){
+            console.error('Error fetching refresh Token',error);
+        }
+    }
+    if(!apiClient)
+    {
+        throw new Error('Failed to initialize API client');
+    }
+    return apiClient;
+
+}
+
+export async function fetchApiClient():Promise<ReturnType<typeof defineOneEntry>>{
+    if(!apiClient)
+    {
+        await setupApiClient();
+    }
+    if(!apiClient)
+    {
+        throw new Error('Api client is still null after setting up');
+    }
+    return apiClient;
+}
